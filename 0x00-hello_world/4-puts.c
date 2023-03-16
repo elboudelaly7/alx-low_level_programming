4dcdd6b9fdcef1ae58e0223fd8c045ce1bf5e502
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-03-16 11:46:30 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- * main -Print a line of code using puts
- *
- * Return: 0
-*/
-
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
