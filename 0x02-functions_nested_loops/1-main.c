cc710fce3a15d49edbc5af4c147329e046f1a08d
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-03-21 10:08:50 -0700

    alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * main - check the code 
  * Return: Always 0.
  */
+
 int main(void)
 {
 	print_alphabet();
