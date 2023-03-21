d08ef20c002bba7f337fa6ba141921ac1ca5368f
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-03-21 15:53:15 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,12 +15,14 @@ int main(void)
 	while (i < 50)
 	{
 	if (i == 0)
+	printf("%d", j);
+	else if (i == 1)
 	printf(", %ld", k);
 	else
 	{
 	k += j;
 	j = k - j;
-	printf(",%ld", k);
+	printf(", %ld", k);
 	}
 	++i;
 	}
