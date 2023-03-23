4a092960002ffb2e840e235b9f4bf380213a9c66
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-03-23 07:53:31 -0700

    Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
 /**
  * mul - Multiplies two integers
  * @x: The first number to multiply
- * @x: The second number to multiply with first number
+ * @y: The second number to multiply with first number
  * Return: The result of multiplication between x param and y param
  */
 
