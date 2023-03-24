d09ebd31d85ec16d680edeaf55cfae1dd1445bfc
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-03-23 19:19:18 -0700

    Like, comment, subscribe

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,18 @@
-#ifndef MAIN_H
+#include "main.h"
 
-#define MAIN_H
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
 
-#include <stdio.h>
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
+		printf("%d is negative\n", i);
+	}
+	else
+		printf("%d is zero\n", i);
 
-#endif /* MAIN_H */
+}
