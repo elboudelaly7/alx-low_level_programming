200bbe8864cf8bc7e2ef5a101d12690ac257e7a3
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-03-23 20:22:16 -0700

    anuubis

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,19 +1,2 @@
-#include "main.h"
-
-void positive_or_negative(int i)
-{
-	if (i > 0)
-	{
-
-		printf("%d is positive\n", i);
-	}
-	else if (i < 0)
-	{
-
-		printf("%d is negative\n", i);
-	}
-	else
-		printf("%d is zero\n", i);
-
-}
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
