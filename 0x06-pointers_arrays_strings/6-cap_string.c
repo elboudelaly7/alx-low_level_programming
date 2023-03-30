7878bce0581375ce6c778bb6fa562b88a043512c
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-03-30 09:22:32 -0700

    anuubis

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,6 @@
  */
 
 char *cap_string(char *str)
-
 {
 
 	int index = 0;
@@ -22,7 +21,7 @@ char *cap_string(char *str)
 	str[index - 1] == '\n' ||
 	str[index - 1] == ',' ||
 	str[index - 1] == ';' ||
-	str[index - 1] == ',' ||
+	str[index - 1] == '.' ||
 	str[index - 1] == '!' ||
 	str[index - 1] == '?' ||
 	str[index - 1] == '"' ||
@@ -30,7 +29,7 @@ char *cap_string(char *str)
 	str[index - 1] == ')' ||
 	str[index - 1] == '{' ||
 	str[index - 1] == '}' ||
-	index == 0);
+	index == 0)
 	str[index] -= 32;
 	index++;
 	}
