97c91a9aec372f6249f70af321ff741d42add469
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-04-11 15:20:27 -0700

    anuubis

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+
+ * create_array - el boudelaly,
+
+ * barcelona best club in world.
+
+ * @size: size of array
+
+ * @c: char to assign
+
+ * Return: pointer to array, NULL if fail
+ 
+ */
+
+char *create_array(unsigned int size, char c)
+
+{
+
+	char *m = malloc(size);
+
+	if (size == 0 || m == 0)
+		return (0);
+
+	while (size--)
+		m[size] = c;
+
+	return (m);
+
+}
