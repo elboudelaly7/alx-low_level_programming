15741acee66aac218db316156da1828a677a72f4
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-04-11 15:56:00 -0700

    anuubis

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include "main.h"
+
+/**
+
+ * _strdup - elboudelaly elmahdi
+
+ * @str: char
+
+ * Return: 0
+
+*/
+
+char *_strdup(char *str)
+
+{
+
+	char *aaa;
+
+	int i, n = 0;
+
+
+
+	if (str == NULL)
+
+		return (NULL);
+
+	i = 0;
+
+	while (str[i] != '\0')
+
+		i++;
+
+
+
+	aaa = malloc(sizeof(char) * (i + 1));
+
+
+
+	if (aaa == NULL)
+
+		return (NULL);
+
+
+
+	for (n = 0; str[r]; n++)
+
+		aaa[n] = str[n];
+
+
+
+	return (aaa);
+
+}
+
