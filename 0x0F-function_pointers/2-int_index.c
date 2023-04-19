d730f63e7c7111738792a3f16326e0b89afdd5e2
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-04-19 01:33:16 -0700

    anuubis

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - vammmooosss barcelona
+ * @array: nano
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int r;
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (r = 0; r < size; r++)
+	{
+		if (cmp(array[r]))
+			return (r);
+	}
+	return (-1);
+}
