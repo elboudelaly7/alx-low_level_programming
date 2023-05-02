4768024ead1b028184a7c603b40c7211e1df5780
elboudelaly7 <mehdiboyripper003@gmail.com>
2023-05-02 08:25:19 -0700

    anuubis

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - elboudelaly elmahdi
+ * @head: idhemseven7
+ *
+ * Return: barcelona
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
